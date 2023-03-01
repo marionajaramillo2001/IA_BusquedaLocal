c27a89e3037fca4bd2449c5b5cc3c9832c832b4f
marionajaramillo <mariona.jaramillo@estudiantat.upc.edu>
2023-03-01 12:03:18 +0100

    first commit

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"IA.Comparticion","l":"TestComparticion"},{"p":"IA.Comparticion","l":"Usuario"},{"p":"IA.Comparticion","l":"Usuarios"}];updateSearchResults();
